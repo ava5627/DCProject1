5e9c50c6d90c6171d4b41dc76def539d0fc16917
austin5627 <austinsharris@gmail.com>
2023-02-21 19:43:57 -0600

    done

--- launcher.sh
+++ launcher.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Change this to your netid
-#netid=ash170000
-netid=rdc180001
+netid=ash170000
+#netid=rdc180001
 
 # Root directory of your project
-#PROJECT_DIR=$HOME/DistributedComputing/Project1
-PROJECT_DIR=/home/013/r/rd/rdc180001/DCProject1
+PROJECT_DIR=/home/013/a/as/ash170000/CS-6380/Project1
+#PROJECT_DIR=/home/013/r/rd/rdc180001/DCProject1
 
 # Directory where the config file is located on your local system
 CONFIG_LOCAL=./config.txt
 CONFIG_REMOTE=$PROJECT_DIR/config.txt
 
 # Directory your java classes are in
-BINARY_DIR=$PROJECT_DIR/cmake-build-debug
+BINARY_DIR=$PROJECT_DIR/cmake-build-debug-remote
 
 # Your main project class
-PROGRAM=Project1
+PROGRAM=$BINARY_DIR/Project1
 
 n=0
 
@@ -29,7 +29,7 @@ cat $CONFIG_LOCAL | sed -e "s/#.*//" | sed -e "/^\s*$/d" |
     read line
     p=$( echo $line | awk '{ print $1 }' )
     host=$( echo $line | awk '{ print $2 }' )
-    kitty --hold -e ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $netid@$host $BINARY_DIR/$PROGRAM $p $CONFIG_REMOTE &
+    kitty --hold -e ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no $netid@$host "stdbuf -o0 $PROGRAM $p $CONFIG_REMOTE" &
     n=$(( n + 1 ))
   done
 )
